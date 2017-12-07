4823133536a5174be6692bfca5d0b6ea87a39e2b
Thunder21907 <thunder21907@gmail.com>
2017-12-07 09:13:13 +0200

    Θανασης
    Το project ειναι ετοιμο για τη σημερηνη παρουσιαση

--- HealthAndCareOffice/ScedullerControlLibrary/ScedullerTable.cs
+++ HealthAndCareOffice/ScedullerControlLibrary/ScedullerTable.cs
@@ -15,7 +15,7 @@ namespace ScedullerControlLibrary
         private Brush brush;
         private Pen pen;
         private int x = 0, y = 0, width, height = 2900;
-        private string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        private string[] days = {"Time", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 
         public ScedullerTable(int width)
         {
@@ -29,19 +29,44 @@ namespace ScedullerControlLibrary
             for(int i = 0; i< days.Length;i++)
             {
                 SizeF size = e.Graphics.MeasureString(days[i], font);
-                int xmidle = (int)((((width / 8 * (i + 1)) + (width / 8 * (i + 2))) / 2) - size.Width / 2);
+                int xmidle = (int)((((width / days.Length * (i)) + (width / days.Length * (i + 1))) / 2) - size.Width / 2);
                 e.Graphics.DrawString(days[i], font, brush, new Point(xmidle,0));
             }
-
+            string time = "00:00";
+            string h, m;
+            int hours = 0;
+            int mins = 0;
             for(int i = 0; i<24*60/15+30;i++)
             {
                 e.Graphics.DrawLine(pen,0, i * 30, width, i * 30);
+                if(i>0)
+                {
+                    mins += 15;
+                    if(mins == 60)
+                    {
+                        mins = 0;
+                        hours++;
+                    }
+                    if(hours<10) h ="0"+ hours + "";
+                    else h = hours + "";
+
+                    if (mins < 10) m = "0" + mins + "";
+                    else m = mins + "";
+
+                    time = h + ":" + m;
+                    SizeF size = e.Graphics.MeasureString(time, font);
+                    int xmidle = (int)(width/days.Length/2-size.Width/2);
+                    e.Graphics.DrawString(time, font, brush, new Point(xmidle, i * 30+10));
+                }
+                
             }
 
             for (int i = 0; i < 9; i++)
             {
                 e.Graphics.DrawLine(pen, i*(width/8), 0, i * (width / 8), height);
             }
+
+
         }
 
 
