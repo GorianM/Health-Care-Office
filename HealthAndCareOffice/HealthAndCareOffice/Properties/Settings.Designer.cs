fbb9357bc2dbc215f97ac6c65b3dcedfaf7849a2
kostasgd <konstantinos.georgiadis@windowslive.com>
2017-12-27 18:03:30 +0200

    Connection

--- HealthAndCareOffice/HealthAndCareOffice/Properties/Settings.Designer.cs
+++ HealthAndCareOffice/HealthAndCareOffice/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HealthAndCareOffice.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,17 @@ namespace HealthAndCareOffice.Properties {
             }
             set {
                 this["Remme"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\Vasi-Diaxeirisis-Ia" +
+            "treiou.accdb")]
+        public string Vasi_Diaxeirisis_IatreiouConnectionString {
+            get {
+                return ((string)(this["Vasi_Diaxeirisis_IatreiouConnectionString"]));
             }
         }
     }
